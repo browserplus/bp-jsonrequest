f56c4be46b414ba6dc527c48e3d54c572a99ba66
Gary MacDonald <garymd@yahoo-inc.com>
2010-08-24 13:10:54 -0700

    port ndmanvar's jsonrequest tests -- disabled until interpreted services are unit-testable

--- unittest/unittest.rb
+++ unittest/unittest.rb
@@ -7,6 +7,8 @@ require 'test/unit'
 require 'open-uri'
 require 'rbconfig'
 include Config
+require 'webrick'
+include WEBrick
 
 class TestJSONRequest < Test::Unit::TestCase
   def setup
@@ -16,14 +18,120 @@ class TestJSONRequest < Test::Unit::TestCase
     end
     @cwd = File.dirname(File.expand_path(__FILE__))
     @service = File.join(@cwd, "../#{subdir}")
+    @server = HTTPServer.new(:Port => 0,
+                             :Logger => WEBrick::Log.new('/dev/null'),
+                             :AccessLog => [nil],
+                             :BindAddress => "127.0.0.1")
+    @urlLocal = "http://localhost:#{@server[:Port]}/"
   end
   
   def teardown
   end
 
+  def private_setup(s)
+    @i = s.allocate(@urlLocal)
+    # Testcase 1.
+    f = "./cases/case1.rb"
+    require f
+
+    @temp = f
+    @temp[".rb"] = ".json"
+    @json = JSON.parse(File.read(@temp))
+
+    if @json["url"] == ""
+      @url = @urlLocal
+    else
+      @url = json["url"]
+    end
+
+    # Need to test send object ALSO.
+    @path1 = File.join(@cwd, "test_files", @json["send"])
+
+    @server.mount("/", SH)
+    t = Thread.new() { @server.start }
+
+    @wantfile = @temp
+    @wantfile[".json"] = ""
+    @wantjson = JSON.parse(File.read(File.join(@wantfile, "doGET.json")))
+  end
+
   def test_load_service
     # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
     #BrowserPlus.run(@service) { |s|
+    #}
+  end
+
+
+  # Require whatever server you want to, mount it,
+  # and then verify that it has the same keys and
+  # values as in your doGET.json and doPOST.json.
+  def test_serverSH_1
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  r = @i.get({ "url" => @url })
+    #  want = @wantjson['graham']
+    #  got = r['graham']
+    #  assert_equal(want, got)
+    #  want = @wantjson['nutella']
+    #  got = r['nutella']
+    #  assert_equal(want, got)
+    #}
+  end
+
+  def test_serverSH_2
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  r = @i.get({ "url" => @url, 'timeout' => @json["timeout"] })
+    #  want = @wantjson['graham']
+    #  got = r['graham']
+    #  assert_equal(want, got)
+    #  want = @wantjson['nutella']
+    #  got = r['nutella']
+    #  assert_equal(want, got)
+    #}
+  end
+
+  def test_serverSH_3
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  r = @i.post({ "url" => @url, 'send' => @path1 })
+    #  want = JSON.parse(File.read(File.join(@wantfile, "doPOST.json")))['post']
+    #  got = r['post']
+    #  assert_equal(want, got)
+    #}
+  end
+
+  def test_serverSH_4
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  r = @i.post({ "url" => @url, 'send' => @path1, 'timeout' => @json["timeout"] })
+    #  want = JSON.parse(File.read(File.join(@wantfile, "doPOST.json")))['post']
+    #  got = r['post']
+    #  assert_equal(want, got)
+    #}
+  end
+
+  def test_serverSH_5
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  # Negative test cases.
+    #  @server.mount("/", OnlyPOST)
+    #  assert_raise(RuntimError) { r = @i.get({ "url" => @url }) }
+    #}
+  end
+
+  def test_serverSH_6
+    # NEEDSWORK!!!  Need to figure out how to host RubyInterpreter to get these tests running
+    #BrowserPlus.run(@service) { |s|
+    #  private_setup(s)
+    #  # Negative test cases.
+    #  @server.mount("/", OnlyGET)
+    #  assert_raise(RuntimeError) { r = @i.post({ "url" => @url }) }
     #}
   end
 end
